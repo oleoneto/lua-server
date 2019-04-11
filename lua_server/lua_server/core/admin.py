e9e05befc293db79031987e6f096620d5b9a5215
Leo <oleoneto@gmail.com>
2019-04-11 10:36:47 -0400

    Remove admin.py

--- lua_server/lua_server/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-admin.site.site_header = "Lua Learning Platform"
-admin.site.site_title = "Lua Learning Platform"
-
-# Admin configuration coming from `admin` package
-from .admin import *
