a8a3339b21392c3a76aecf9b02e5e2dc3fb42941
Leo <oleoneto@gmail.com>
2019-04-11 10:34:34 -0400

    Update core urls

--- lua_server/lua_server/core/urls.py
+++ lua_server/lua_server/core/urls.py
@@ -1,31 +1,25 @@
 from django.urls import path, include
-from rest_framework import routers
 from rest_framework_swagger.views import get_swagger_view
-from .viewsets.user import UserViewSet
-from .viewsets.post import PostViewSet
-from .views.index import IndexView
-from .views.documentation import DocumentationView
-from .views.faq import FAQView
-from .views.pricing import PricingView
-from .views.about import AboutView
+from .views import *
+from .viewsets import router
 
-router = routers.SimpleRouter(trailing_slash=False)
 api_schema_docs = get_swagger_view(title='Lua LMS API')
-
-router.register('users', UserViewSet)
-router.register('posts', PostViewSet)
 
 urlpatterns = [
     # REST API Endpoints
     path('api/v1/', include(router.urls)),
-    path('api/auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api/docs/', api_schema_docs),
-    path('api/documentation/', api_schema_docs),
+    path('api/v1/auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/v1/docs/', api_schema_docs),
+    path('api/v1/documentation/', api_schema_docs),
+
+    # TODO: Implement templates
+    # path('accounts/', include('django_registration.backends.activation.urls')),
 
     # Pages
     path('', IndexView.as_view(), name='index'),
-    path('about', AboutView.as_view(), name='about'),
-    path('documentation', DocumentationView.as_view(), name='documentation'),
-    path('faq', FAQView.as_view(), name='faq'),
-    path('pricing', PricingView.as_view(), name='pricing'),
+    path('about/', AboutView.as_view(), name='about'),
+    path('documentation/', DocumentationView.as_view(), name='documentation'),
+    path('faq/', FAQView.as_view(), name='faq'),
+    path('pricing/', PricingView.as_view(), name='pricing'),
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),
 ]
