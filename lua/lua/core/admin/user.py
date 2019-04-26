416ecef37e134856dd00154ebc0ec61998bbb395
Leo <oleoneto@gmail.com>
2019-04-26 12:10:15 -0400

    Update user admin fields

--- lua/lua/core/admin/user.py
+++ lua/lua/core/admin/user.py
@@ -1,27 +1,17 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin, UserChangeForm, UserCreationForm, AdminPasswordChangeForm
 from ..models.user import User
-from .inlines.enrollment import EnrollmentInline
 
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
     model = User
-    inlines = [EnrollmentInline]
     list_display = ['id', 'username', 'name', 'is_staff']
     prepopulated_fields = {'username': ('first_name', 'last_name',)}
     form = UserChangeForm
     add_form = UserCreationForm
     change_password_form = AdminPasswordChangeForm
-    readonly_fields = ('internal_email',)
-
-    fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('first_name', 'last_name', 'username', 'email', 'internal_email', 'password', 'photo')
-        }),
-        ('Permissions', {'fields': ('is_staff', 'roles')}),
-    )
+    readonly_fields = ('internal_email', 'date_joined', 'last_login',)
 
     add_fieldsets = (
         (None, {
