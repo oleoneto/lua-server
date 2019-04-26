30cbda0e172f09077412a77f1adb608cb2c791bd
Leo <oleoneto@gmail.com>
2019-04-26 12:19:22 -0400

    Add gradebook entry types

--- lua/lua/core/models/gradebook.py
+++ lua/lua/core/models/gradebook.py
@@ -2,14 +2,14 @@ from django.db import models
 from .helpers.identifier import make_identifier
 from .user import User
 from .course import Course
+from .assignment import Assignment
 
 
 class Gradebook(models.Model):
     id = models.BigIntegerField(primary_key=True, editable=False)
     student = models.ForeignKey(User, related_name='gradebooks', on_delete=models.DO_NOTHING)
     course = models.ForeignKey(Course, related_name='gradebooks', on_delete=models.DO_NOTHING)
-    
-    # Default fields. Omit with the --no-defaults flag
+
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
 
@@ -24,3 +24,46 @@ class Gradebook(models.Model):
     
     def __str__(self):
         return f'#{self.id}'
+
+
+class GradebookAssignmentEntry(models.Model):
+    id = models.BigIntegerField(primary_key=True, editable=False)
+    gradebook = models.ForeignKey(Gradebook, related_name='assignment_entries', on_delete=models.DO_NOTHING)
+    assignment = models.ForeignKey(Assignment, related_name='gradebook_entries', on_delete=models.DO_NOTHING)
+    grade = models.PositiveIntegerField()
+
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+
+    class Meta:
+        db_table = 'gradebook_assignment_entries'
+        ordering = ['-created_at']
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.id = make_identifier()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'Assignment entry #{self.id}'
+
+
+class GradebookNoteEntry(models.Model):
+    id = models.BigIntegerField(primary_key=True, editable=False)
+    gradebook = models.ForeignKey(Gradebook, related_name='note_entries', on_delete=models.DO_NOTHING)
+    note = models.TextField(max_length=700)
+
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+
+    class Meta:
+        db_table = 'gradebook_note_entries'
+        ordering = ['-created_at']
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.id = make_identifier()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'Note #{self.id}'
