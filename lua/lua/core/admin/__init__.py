b374e04f2461d3339e2aaeb4b4c378554e276bef
Leo <oleoneto@gmail.com>
2019-04-27 15:32:21 -0400

    Add branding header and title + fix imports

--- lua/lua/core/admin/__init__.py
+++ lua/lua/core/admin/__init__.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+
+admin.site.site_header = "Olhares + Lua LMS"
+admin.site.site_title = "Lua Learning Management System"
+
 from .user import UserAdmin
 from .event import EventAdmin
 from .instructor import InstructorAdmin
@@ -6,15 +10,15 @@ from .logs import LogEntryAdmin
 from .study_plan import StudyPlanAdmin
 from .student import StudentAdmin
 from .post import PostAdmin
-from .enrollment import Enrollment
-from .course import Course
-from .comment import Comment
-from .role import Role
-from .term import Term
-from .waitlist import Waitlist
-from .gradebook import Gradebook
-from .assignment import Assignment, AssignmentType
-from .learning_objective import LearningObjective
-from .learning_level import LearningLevel
-from .course_offer import CourseOffer
+from .enrollment import EnrollmentAdmin
+from .course import CourseAdmin
+from .comment import CommentAdmin
+from .role import RoleAdmin
+from .term import TermAdmin
+from .waitlist import WaitlistAdmin
+from .gradebook import GradebookAdmin
+from .assignment import AssignmentAdmin, AssignmentTypeAdmin
+from .learning_objective import LearningObjectiveAdmin
+from .learning_level import LearningLevelAdmin
+from .course_offer import CourseOfferAdmin
 from .caderneta import CadernetaAdmin
