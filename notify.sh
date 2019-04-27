c3c88788e71fdd0ed8951ab917bdd3ad09666f47
Leo <oleoneto@gmail.com>
2019-04-27 18:27:45 -0400

    Update deployment commands

--- notify.sh
+++ notify.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Notify to Slack
 
-PAYLOAD="$1" && curl -X POST -H 'Content-type: application/json' --data '{"text": "$PAYLOAD" }' https://hooks.slack.com/services/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
+PAYLOAD="$1" && curl -X POST -H 'Content-type: application/json' --data '{"text": "\$PAYLOAD\" }' https://hooks.slack.com/services/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
