9e781cbcb47d1707ad93c9151017209d31f3aeec
Leo <oleoneto@gmail.com>
2019-04-27 10:50:59 -0400

    Update IsInCourseOrNoAccess permission

--- lua/lua/core/viewsets/permissions/is_member.py
+++ lua/lua/core/viewsets/permissions/is_member.py
@@ -27,10 +27,10 @@ class IsInCourseNoAccess(permissions.IsAuthenticated):
     def has_object_permission(self, request, view, obj):
         try:
             instructor = Instructor.objects.get(user=request.user)
-            return obj.course_instructor == instructor
         except Instructor.DoesNotExist:
-            try:
-                student = Student.objects.get(user=request.user)
-                return obj.students.filter(student_id=student.id)
-            except Student.DoesNotExist:
-                return False
+            instructor = False
+        try:
+            student = Student.objects.get(user=request.user)
+        except Student.DoesNotExist:
+            student = False
+        return obj.students.filter(student=student) or obj.course_instructor == instructor
