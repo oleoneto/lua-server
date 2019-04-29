8f8aff7ba2f9a3b14e9b7e2474a5db8666e4dc19
Leo <oleoneto@gmail.com>
2019-04-29 13:14:04 -0400

    Pre-fix for DataError on Enrollment.save()

--- lua/lua/core/models/enrollment.py
+++ lua/lua/core/models/enrollment.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.exceptions import ValidationError
+from django.db.utils import DataError
 from .helpers.identifier import make_identifier
 from .helpers.mailer import send_mail
 from .student import Student
@@ -23,8 +24,10 @@ Admin @ LuaLMS
 
 class Enrollment(models.Model):
     id = models.BigIntegerField(primary_key=True, editable=False)
-    student = models.ForeignKey(Student, related_name='enrollments', on_delete=models.DO_NOTHING)
-    course_offer = models.ForeignKey(CourseOffer, related_name='enrollments', on_delete=models.DO_NOTHING)
+    student = models.ForeignKey(Student, related_name='enrollments',
+                                on_delete=models.DO_NOTHING)
+    course_offer = models.ForeignKey(CourseOffer, related_name='enrollments',
+                                     on_delete=models.DO_NOTHING)
 
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
@@ -44,18 +47,29 @@ class Enrollment(models.Model):
         if not self.id:
             self.id = make_identifier()
             will_notify = True
+
+        # Course is full, add student to waitlist
         if self.course_offer.enrollments.count() >= self.course_offer.enrollment_limit:
-            Waitlist.objects.get_or_create(student=self.student, course_offer=self.course_offer)
+            Waitlist.objects.get_or_create(student=self.student,
+                                           course_offer=self.course_offer)
             print(f"Course if full. Student {self.student.username} added to waitlist.")
             return
-        super().save(*args, **kwargs)
+        try:
+            super().save(*args, **kwargs)
+        except DataError as error:
+            raise DataError(f'Failed to save.\n Reason: {error.args[0]}')
 
         # Create Gradebook for user
         Gradebook.objects.get_or_create(student=self.student, course_offer=self.course_offer)
 
         if will_notify:
-            send_mail(user=self.student.user, subject=f"Lua: Enrollment confirmation #{self.id}",
-                      message=f'{ENROLLMENT_MESSAGE.format(self.student.name, self.course_offer.course, self.course_offer.course.id, self.student.gradebooks.last(), self.id)}')
+            send_mail(user=self.student.user,
+                      subject=f"Lua: Enrollment confirmation #{self.id}",
+                      message=f"""{ENROLLMENT_MESSAGE.format(self.student.name,
+                                                             self.course_offer.course, 
+                                                             self.course_offer.course.id,
+                                                             self.student.gradebooks.last(), 
+                                                             self.id)}""")
 
         try:
             # Attempt to remove user from waiting list if enrollment is successful
@@ -71,7 +85,8 @@ class Enrollment(models.Model):
         if waiting_instance:
             try:
                 # Attempt to enroll first user in waiting list
-                Enrollment.objects.create(student=waiting_instance.student, course_offer=waiting_instance.course_offer)
+                Enrollment.objects.create(student=waiting_instance.student,
+                                          course_offer=waiting_instance.course_offer)
                 waiting_instance.delete()
             except Waitlist.DoesNotExist:
                 return
