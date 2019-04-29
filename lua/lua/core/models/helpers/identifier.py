3977165861630fc763013b4fe9287ab900b636a9
Leo <oleoneto@gmail.com>
2019-04-29 15:41:23 -0400

    Update make_identifier

--- lua/lua/core/models/helpers/identifier.py
+++ lua/lua/core/models/helpers/identifier.py
@@ -1,18 +1,19 @@
 import random
 import time
 
-
-START_TIME = int(time.time()*1000)
+OFFSET = 0.01
+START_TIME = int(time.time()*OFFSET)
+BITS = 16
 
 
 def make_identifier():
 	# https://stackoverflow.com/a/37605582/7899348
 
-	t = int(time.time()*1000) - START_TIME
-	u = random.SystemRandom().getrandbits(23)
-	return (t << 23) | u
+	t = int(time.time()*OFFSET) - START_TIME
+	u = random.SystemRandom().getrandbits(BITS)
+	return (t << BITS) | u
 
 
 def reverse_identifier(identifier):
-	t = identifier >> 23
+	t = identifier >> BITS
 	return t + START_TIME
