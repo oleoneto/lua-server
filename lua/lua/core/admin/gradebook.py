73887482d4893a5a32b88bfbf41cab8ca21f2526
Leo <oleoneto@gmail.com>
2019-05-03 13:06:01 -0400

    Show Gradebook in admin

--- lua/lua/core/admin/gradebook.py
+++ lua/lua/core/admin/gradebook.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from ..models.gradebook import Gradebook
 
 
-# @admin.register(Gradebook)
+@admin.register(Gradebook)
 class GradebookAdmin(admin.ModelAdmin):
     pass
