bcb6a43adf638746ed67dec6ca34fdb4a3c6ba57
Leo <oleoneto@gmail.com>
2019-06-16 19:01:25 -0400

    Add URL in Slack notification

--- notify.sh
+++ notify.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -X POST -H 'Content-type: application/json' --data '{"text": "Lua LMS is Live" }' https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data '{"text": "Lua LMS deployed successfully to:\nhttps://lualms.com" }' https://hooks.slack.com/services/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
