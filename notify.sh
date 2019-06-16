c54dcec0b34d8bd5a62d7e4c8de8d33c32962b9b
Leo <oleoneto@gmail.com>
2019-06-16 18:56:58 -0400

    Update release and notify scripts

--- notify.sh
+++ notify.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-# Notify to Slack
 
-PAYLOAD="$1" && curl -X POST -H 'Content-type: application/json' --data '{"text": "\$PAYLOAD\" }' https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data '{"text": "Lua LMS is Live" }' https://hooks.slack.com/services/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
